86cda6b29921ce7cb925039ed8916c47598be19b
Hypario <fabienduterte@mailfence.com>
2019-12-18 10:06:32 +0100

    landing page

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Serie;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    /**
+     * Landing page !
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index() {
-        return view("index");
+        $series = Serie::all();
+
+        return view("index", compact("series"));
     }
 }
