e66e4680564173f7132149a86d40c8670e28aee9
Hypario <fabienduterte@mailfence.com>
2019-12-19 14:25:12 +0100

    Back : now able to register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,6 +14,19 @@
                         <div class="form-group row">
 
                             <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" placeholder="Votre nom d'utilisateur" value="{{ old('name') }}" required>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+
+                            <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Saisissez votre adresse mail..." value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
@@ -22,10 +35,12 @@
                                     </span>
                                 @enderror
                             </div>
+                        </div>
+
                         <div class="form-group row">
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Saisissez votre mot de passe" required autocomplete="new-password">
+                                <input id="password" type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Saisissez votre mot de passe" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -38,7 +53,7 @@
                         <div class="form-group row">
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" placeholder="Confirmer votre mot de passe" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirmer votre mot de passe" required autocomplete="new-password">
                             </div>
                         </div>
 
