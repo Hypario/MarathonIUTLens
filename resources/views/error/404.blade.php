4ce89121b8583bdca36171d9692b4a3dcbd5251e
Thomas Dignoire <tdignoire@protonmail.com>
2019-12-19 09:47:10 +0100

    Update 404 with place holder

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -1,1 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Seeries - 404</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/error.css') }}" rel="stylesheet">
+</head>
+<body>
+</body>
+</html>
