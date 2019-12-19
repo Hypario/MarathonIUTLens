6fdac0e0c6229a778983a9d6022c69c1928ea843
Hypario <fabienduterte@mailfence.com>
2019-12-19 12:44:47 +0100

    Back : reformat code

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -3,24 +3,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Episodde;
 use App\Episode;
-use App\Serie;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class EpisodeController extends Controller
 {
 
-    public function see($id) {
-        if ($myUser = Auth::user()) {
-            $episode = Episode::all()->where("id", "=", $id);
-
-
-            DB::table("seen")->insert(["user_id"=>$myUser->id, "episode_id"=>$id]);
+    public function see($id)
+    {
+        if (Auth::check()) {
+            if ($episode = Episode::find($id)) {
+                DB::table("seen")->insert(["user_id" => Auth::user()->id, "episode_id" => $id]);
+            }
             return redirect()->back();
-        } else {
-            echo "L'affichage de cette page n'est pas normal";
         }
         return redirect('404');
     }
