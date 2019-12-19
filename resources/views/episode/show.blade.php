6a32cca48b785b9eac39fad9d630472e297153cc
Thomas Dignoire <tdignoire@protonmail.com>
2019-12-19 15:21:30 +0100

    Update CSS Episode

--- resources/views/episode/show.blade.php
+++ resources/views/episode/show.blade.php
@@ -2,53 +2,61 @@
 @section('title',$serie->nom,'Episode n°',$episode->numero)
 
 @section('content')
-    <di>
-        <a href="{{ route('serie.show', [$serie->id]) }}">retour à la série.</a>
-    </di>
+    <div id="retourSerie">
+        <a href="{{ route('serie.show', [$serie->id]) }}" style="color: #115557;">retour à la série</a>
+    </div>
     <div>
-        <p>{{$serie->nom}}, saison {{$episode->saison}}</p>
-        <p>Episode n°{{$episode->numero}}; {{$episode->nom}}.</p>
+        <p ><strong class="titre">{{$serie->nom}}</strong></p>
+        <p class="titre"> saison {{$episode->saison}} Episode n°{{$episode->numero}}, {{$episode->nom}}.</p>
     </div>
     <div>
         <p>
             @if ($episode->urlImage)
-                <img src={{ url($episode->urlImage) }} />
+                <img src={{ url($episode->urlImage) }} id="imageEpisode"/>
+            @else
+                <div id="imageEpisode" class="textImage">Pas de preview disponible.</div>
             @endif
         </p>
     </div>
-    @if(Auth::check())
+
+    <div id="infoEpisode">
+
         <div>
-            <p>
-                <b>Appréciation de la série : </b>
-                @if($isEpisodeSeen)
-                    Vous avez déjà vu l'épisode
-                @else
-                    <a href="{{ route('episode.see',$episode->id) }}">Voir l'épisode</a>
-                @endif
-            </p>
+            @if($episode->resume)
+                <p><strong>Résumé : </strong>{!! html_entity_decode($episode->resume)!!}</p>
+            @else
+                <p><strong>Résumé : </strong>Pas de résumé disponible.</p>
+            @endif
+
         </div>
-    @endif
-    <div>
-
-        <p><strong>Résumé : </strong>{!! html_entity_decode($episode->resume)!!}</p>
-    </div>
-    <div>
-
-        <p><strong>Durée : </strong>{{$episode->duree}} minutes.</p>
-    </div>
-
-    <div>
-        <p><strong>Première : </strong>{{$episode->premiere}}</p>
+        <div>
+            <p><strong>Durée : </strong>{{$episode->duree}} minutes.</p>
+        </div>
+        <div>
+            <p><strong>Première : </strong>{{$episode->premiere}}</p>
+        </div>
+        @if(Auth::check())
+            <div>
+                <p>
+                    <b>Appréciation de la série : </b>
+                    @if($isEpisodeSeen)
+                        Vous avez déjà vu l'épisode
+                    @else
+                        <a href="{{ route('episode.see',$episode->id) }}">Voir l'épisode</a>
+                    @endif
+                </p>
+            </div>
+        @endif
     </div>
 
 
 
     @if ($previous)
-        <a href="{{ route('episode.show', [$serie->id, $previous]) }}">Épisode précédent</a>
+        <a href="{{ route('episode.show', [$serie->id, $previous]) }}" id="previousEp">Épisode précédent</a>
     @endif
 
     @if ($next)
-        <a href="{{ route('episode.show', [$serie->id, $next]) }}">Épisode suivant</a>
+        <a href="{{ route('episode.show', [$serie->id, $next]) }}" id="nextEP">Épisode suivant</a>
     @endif
 
 @endsection
