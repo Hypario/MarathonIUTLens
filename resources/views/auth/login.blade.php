e3ebaf5e8a999313641308c5bd88d4c945638b7f
Thomas Dignoire <tdignoire@protonmail.com>
2019-12-19 11:12:09 +0100

    Front : adding CSS to login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Connexion') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" id="inscr">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
